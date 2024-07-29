5329600ecac3742ef3780cfc6b50f364c7b31cc3
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2024-07-30 02:54:41 +0800

    initial deployment of prisma
    * currently working on my json shoe file

---
+++ prisma/migrations/20240729183642_init/migration.sql
@@ -0,0 +1,127 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('CUSTOMER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "Shoes" (
+    "id" SERIAL NOT NULL,
+    "name_id" TEXT,
+    "name" TEXT NOT NULL,
+    "brand" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "discount" DOUBLE PRECISION,
+    "rating" DOUBLE PRECISION,
+    "img_URL" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Shoes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "new_arrival" BOOLEAN NOT NULL DEFAULT true,
+    "hot_item" BOOLEAN NOT NULL,
+    "men" BOOLEAN NOT NULL,
+    "women" BOOLEAN NOT NULL,
+    "kids" BOOLEAN NOT NULL,
+    "shoeId" INTEGER NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "user_name" TEXT NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'CUSTOMER',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "MenShoeSize" (
+    "id" SERIAL NOT NULL,
+    "eur_39" BOOLEAN NOT NULL,
+    "eur_40" BOOLEAN NOT NULL,
+    "eur_41" BOOLEAN NOT NULL,
+    "eur_42" BOOLEAN NOT NULL,
+    "eur_43" BOOLEAN NOT NULL,
+    "eur_44" BOOLEAN NOT NULL,
+    "eur_45" BOOLEAN NOT NULL,
+    "eur_46" BOOLEAN NOT NULL,
+    "eur_47" BOOLEAN NOT NULL,
+    "eur_48" BOOLEAN NOT NULL,
+    "eur_49" BOOLEAN NOT NULL,
+    "categoryId" INTEGER NOT NULL,
+
+    CONSTRAINT "MenShoeSize_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "WomenShoeSize" (
+    "id" SERIAL NOT NULL,
+    "eur_33" BOOLEAN NOT NULL,
+    "eur_34" BOOLEAN NOT NULL,
+    "eur_35" BOOLEAN NOT NULL,
+    "eur_36" BOOLEAN NOT NULL,
+    "eur_37" BOOLEAN NOT NULL,
+    "eur_38" BOOLEAN NOT NULL,
+    "eur_39" BOOLEAN NOT NULL,
+    "eur_40" BOOLEAN NOT NULL,
+    "eur_41" BOOLEAN NOT NULL,
+    "categoryId" INTEGER NOT NULL,
+
+    CONSTRAINT "WomenShoeSize_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "KidsShoeSize" (
+    "id" SERIAL NOT NULL,
+    "eur_30" BOOLEAN NOT NULL,
+    "eur_31" BOOLEAN NOT NULL,
+    "eur_32" BOOLEAN NOT NULL,
+    "eur_33" BOOLEAN NOT NULL,
+    "eur_34" BOOLEAN NOT NULL,
+    "eur_35" BOOLEAN NOT NULL,
+    "eur_36" BOOLEAN NOT NULL,
+    "eur_37" BOOLEAN NOT NULL,
+    "eur_38" BOOLEAN NOT NULL,
+    "categoryId" INTEGER NOT NULL,
+
+    CONSTRAINT "KidsShoeSize_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_shoeId_key" ON "Category"("shoeId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_user_name_key" ON "User"("user_name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "MenShoeSize_categoryId_key" ON "MenShoeSize"("categoryId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "WomenShoeSize_categoryId_key" ON "WomenShoeSize"("categoryId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "KidsShoeSize_categoryId_key" ON "KidsShoeSize"("categoryId");
+
+-- AddForeignKey
+ALTER TABLE "Category" ADD CONSTRAINT "Category_shoeId_fkey" FOREIGN KEY ("shoeId") REFERENCES "Shoes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "MenShoeSize" ADD CONSTRAINT "MenShoeSize_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WomenShoeSize" ADD CONSTRAINT "WomenShoeSize_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "KidsShoeSize" ADD CONSTRAINT "KidsShoeSize_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- routes/index.ts
+++ routes/index.ts
@@ -1,9 +1,88 @@
 import express, { Router, Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client'
+const prisma: PrismaClient = new PrismaClient();
+
 
 export const router: Router = express.Router();
 
-router.get('/', (req: Request, res: Response) => res.send('Welcome to Fresh Kicks API!'));
+router.get('/', async (req: Request, res: Response) =>{
+    res.send('Welcome to Fresh Kicks API!')
 
-router.get('/api', (req: Request, res: Response) => {
-    
+    try{
+        await prisma.shoes.create({
+            data: {
+                name: 'NIKE AIR JORDAN 5 RETRO BASKETBALL SHOES - BLACK',
+                name_id: 'nike-air-jordan-retro-basketball-shoes-black',
+                brand: 'Jordan',
+                price: 8716,
+                img_URL: "https://www.footlocker.ph/media/catalog/product/cache/e81e4f913a1cad058ef66fea8e95c839/0/8/0803-NIKDD0587047005008-1.jpg",
+                category: {
+                    create: {
+                        new_arrival: false,
+                        hot_item: true,
+                        men: true,
+                        menShoeSize:{
+                            create: {
+                                eur_39: true,
+                                eur_40: false,
+                                eur_41: true,
+                                eur_42: false,
+                                eur_43: true,
+                                eur_44: false,
+                                eur_45: true,
+                                eur_46: true,
+                                eur_47: true,
+                                eur_48: true,
+                                eur_49: true
+                            }
+                        },
+                        women: false,
+                        kids: true,
+                        kidsShoeSize:{
+                            create: {
+                                eur_30: false,
+                                eur_31: false,
+                                eur_32: false,
+                                eur_33: true,
+                                eur_34: false,
+                                eur_35: true,
+                                eur_36: false,
+                                eur_37: true,
+                                eur_38: true
+                            }
+                        }
+                    }
+                }
+            }
+        })
+    }catch(error){
+        if (error instanceof Error){
+            res.send(error.message)
+        }else{
+            res.send('This is a user-generated error: There is an error occured somewhere')
+        }
+    }
+});
+
+router.get('/api', async (req: Request, res: Response) => {
+    try{
+        const allShoes = await prisma.shoes.findMany({
+            include: {
+                category: {
+                    include: {
+                        menShoeSize: true,
+                        womenShoeSize: true,
+                        kidsShoeSize: true
+                    }
+                }
+            }
+        })
+        res.json(allShoes)
+    }catch(error){
+        if (error instanceof Error){
+            res.send(error.message)
+        }else{
+            res.send('This is a user-generated error: There is an error occured somewhere')
+        }
+    }
 })
