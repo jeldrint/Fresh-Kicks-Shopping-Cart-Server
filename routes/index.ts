88a78f0b1b478ed222b895cfe058ec7dc74465fe
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2024-08-02 22:03:05 +0800

    manually added other shoe data

--- routes/index.ts
+++ routes/index.ts
@@ -11,12 +11,12 @@ router.get('/', async (req: Request, res: Response) =>{
     // try{
     //     await prisma.shoes.update({
     //         where: {
-    //             id: 10
+    //             id: 20
     //         },
     //         data: {
     //             category: {
     //                 update: {
-    //                     kids: true
+    //                     women: true
     //                 }
     //             }
     //         }
@@ -43,10 +43,10 @@ router.get('/api', async (req: Request, res: Response) => {
                 }
             },
             orderBy: {
-                id: 'asc'
+                id: 'desc'
             }
         })
-        res.json(allShoes)
+        return res.json(allShoes)
     }catch(error){
         if (error instanceof Error){
             res.send(error.message)
@@ -57,61 +57,82 @@ router.get('/api', async (req: Request, res: Response) => {
 })
 
 
-// data: {
-//     name: "NIKE AIR FORCE 1 '07 LV8 SNEAKERS - BABY BLUE",
-//     name_id: 'nike-air-force-lv-sneakers-shoes-baby-blue',
-//     brand: 'Nike',
-//     price: 6895,
-//     discount: 0.15,
-//     img_URL: "https://www.footlocker.ph/media/catalog/product/cache/e81e4f913a1cad058ef66fea8e95c839/0/8/0803-NIKDZ252210000W10H-1.jpg",
-//     category: {
-//         create: {
-//             new_arrival: false,
-//             hot_item: false,
-//             men: true,
-//             menShoeSize:{
-//                 create: {
-//                     eur_39: true,
-//                     eur_40: true,
-//                     eur_41: true,
-//                     eur_42: false,
-//                     eur_43: false,
-//                     eur_44: false,
-//                     eur_45: false,
-//                     eur_46: false,
-//                     eur_47: true,
-//                     eur_48: true,
-//                     eur_49: true,
-//                 }
-//             },
-//             women: true,
-//             womenShoeSize:{
-//                 create: {
-//                     eur_33: true,
-//                     eur_34: true,
-//                     eur_35: true,
-//                     eur_36: false,
-//                     eur_37: false,
-//                     eur_38: false,
-//                     eur_39: false,
-//                     eur_40: false,
-//                     eur_41: true,
-//                 }
-//             },
-//             kids: false,
-//             kidsShoeSize: {
-//                 create: {
-//                     eur_30: true,
-//                     eur_31: true,
-//                     eur_32: false,
-//                     eur_33: false,
-//                     eur_34: false,
-//                     eur_35: true,
-//                     eur_36: false,
-//                     eur_37: true,
-//                     eur_38: true
+/* CUE FOR CREATING A SHOE PROFILE*/
+
+// await prisma.shoes.create({
+//     data: {
+//         name: "AIR MAX 1 BOYS' GRADE SCHOOL SNEAKERS SHOES - WHITE",
+//         name_id: 'air-max-1-boys-grade-school-sneakers-shoes-white',
+//         brand: 'Nike',
+//         price: 4695,
+//         discount: 0.40,
+//         img_URL: "https://www.footlocker.ph/media/catalog/product/cache/1384ea813c36abc3a773dd6494b9b881/0/8/0803-NIKDZ330710700W07Y-2.jpg",
+//         category: {
+//             create: {
+//                 new_arrival: false,
+//                 hot_item: false,
+//                 men: true,
+//                 menShoeSize:{
+//                     create: {
+//                         eur_39: true,
+//                         eur_40: true,
+//                         eur_41: true,
+//                         eur_42: false,
+//                         eur_43: false,
+//                         eur_44: false,
+//                         eur_45: false,
+//                         eur_46: false,
+//                         eur_47: true,
+//                         eur_48: true,
+//                         eur_49: true,
+//                     }
+//                 },
+//                 women: true,
+//                 womenShoeSize:{
+//                     create: {
+//                         eur_33: true,
+//                         eur_34: true,
+//                         eur_35: true,
+//                         eur_36: false,
+//                         eur_37: false,
+//                         eur_38: false,
+//                         eur_39: false,
+//                         eur_40: false,
+//                         eur_41: true,
+//                     }
+//                 },
+//                 kids: true,
+//                 kidsShoeSize: {
+//                     create: {
+//                         eur_30: true,
+//                         eur_31: true,
+//                         eur_32: false,
+//                         eur_33: false,
+//                         eur_34: false,
+//                         eur_35: true,
+//                         eur_36: false,
+//                         eur_37: true,
+//                         eur_38: true
+//                     }
 //                 }
 //             }
 //         }
 //     }
-// }
+// })
+
+
+
+/* CUE FOR UPDATING A SHOE PROFILE (NESTED) */
+
+// await prisma.shoes.update({
+//     where: {
+//         id: 10
+//     },
+//     data: {
+//         category: {
+//             update: {
+//                 kids: true
+//             }
+//         }
+//     }
+// })
